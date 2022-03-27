1972dc8260682afc290631564d32d29d97ef830d
JacobLinCool <jacoblincool@gmail.com>
2022-03-27 23:20:31 +0800

    Update action.js

--- action/action.js
+++ action/action.js
@@ -1,43 +1,3 @@
-var __create = Object.create;
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getOwnPropSymbols = Object.getOwnPropertySymbols;
-var __getProtoOf = Object.getPrototypeOf;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __propIsEnum = Object.prototype.propertyIsEnumerable;
-var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __spreadValues = (a, b) => {
-  for (var prop in b || (b = {}))
-    if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
-  if (__getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
-    }
-  return a;
-};
-var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
-var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
-  get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
-}) : x)(function(x) {
-  if (typeof require !== "undefined")
-    return require.apply(this, arguments);
-  throw new Error('Dynamic require of "' + x + '" is not supported');
-});
-var __reExport = (target, module, copyDefault, desc) => {
-  if (module && typeof module === "object" || typeof module === "function") {
-    for (let key of __getOwnPropNames(module))
-      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
-        __defProp(target, key, { get: () => module[key], enumerable: !(desc = __getOwnPropDesc(module, key)) || desc.enumerable });
-  }
-  return target;
-};
-var __toESM = (module, isNodeMode) => {
-  return __reExport(__markAsModule(__defProp(module != null ? __create(__getProtoOf(module)) : {}, "default", !isNodeMode && module && module.__esModule ? { get: () => module.default, enumerable: true } : { value: module, enumerable: true })), module);
-};
-
 // node_modules/.pnpm/tsup@5.11.13_typescript@4.5.2/node_modules/tsup/assets/esm_shims.js
 import { fileURLToPath } from "url";
 import path from "path";
@@ -46,8 +6,8 @@ var getDirname = () => path.dirname(getFilename());
 var __dirname = /* @__PURE__ */ getDirname();
 
 // src/action/action.ts
-import { execSync } from "child_process";
-import fs from "fs";
+import { execSync } from "node:child_process";
+import fs from "node:fs";
 prepare();
 main();
 function prepare() {
@@ -78,14 +38,14 @@ function prepare() {
   console.log("\n");
 }
 async function main() {
-  const core = (await Promise.resolve().then(() => __toESM(__require("@actions/core"), 1))).default;
+  const core = (await import("@actions/core")).default;
   try {
-    const { BahamutAutomation } = await Promise.resolve().then(() => __toESM(__require("./lib/core"), 1));
+    const { BahamutAutomation } = await import("./lib/core");
     const config_path = core.getInput("config");
-    const secrets = __spreadValues({}, JSON.parse(core.getInput("secrets") || "{}"));
+    const secrets = { ...JSON.parse(core.getInput("secrets") || "{}") };
     let raw = fs.readFileSync(config_path, "utf8");
     for (const key in secrets) {
-      raw = raw.replace(new RegExp(`$${key}`, "g"), secrets[key]);
+      raw = raw.replace(new RegExp(`$${key}`, "ig"), secrets[key]);
     }
     fs.writeFileSync(config_path, raw);
     const automation = BahamutAutomation.from(config_path);
