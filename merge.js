8854f3c0079c49ca654b41e21a91922a507b9302
JacobLinCool <jacoblincool@gmail.com>
2022-05-30 12:14:23 +0000

    feat: add docker usage

---
+++ merge.js
@@ -0,0 +1,12 @@
+const fs = require("node:fs");
+
+const { config, secrets } = process.env;
+const parsed_secrets = JSON.parse(secrets);
+
+let merged = fs.readFileSync(config, "utf8");
+for (const key in parsed_secrets) {
+    const regex = new RegExp("\\$" + key, "g");
+    console.log(`Merging ${merged.match(regex)?.length ?? 0} ${regex} in ${config}`);
+    merged = merged.replace(regex, parsed_secrets[key]);
+}
+fs.writeFileSync("merged-config.yml", merged);
