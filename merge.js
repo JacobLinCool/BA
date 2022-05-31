4f3ef9bd3f2eef92b8dd628c17356f58fc59c6dd
JacobLinCool <jacoblincool@gmail.com>
2022-05-31 09:57:06 +0000

    chore: cleanup old workflows

--- merge.js
+++ merge.js
@@ -6,7 +6,7 @@ const parsed_secrets = JSON.parse(secrets);
 let merged = fs.readFileSync(config, "utf8");
 for (const key in parsed_secrets) {
     const regex = new RegExp("\\$" + key, "g");
-    console.log(`Merging ${merged.match(regex)?.length ?? 0} ${regex} in ${config}`);
+    console.log(`Merging ${merged.match(regex)?.length ?? 0} ${regex}`);
     merged = merged.replace(regex, parsed_secrets[key]);
 }
 fs.writeFileSync("merged-config.yml", merged);
